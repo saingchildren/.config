44849f7624374ec15b33ee4d5713fd2f1c712d30
saingchildren <80457007+saingchildren@users.noreply.github.com>
2024-07-21 09:27:18 +0800

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,28 +1,5 @@
-require('sic.base')
-require('sic.map')
-require('sic.highlight')
-require('sic.packer')
-require('sic.plugin.telescope')
-require('sic.plugin.autopairs')
-require('sic.plugin.cmp')
-require('sic.plugin.lspconfig')
-require('sic.plugin.lspkind')
-require('sic.plugin.lualine')
-require('sic.plugin.mason')
-require('sic.plugin.neosolarized')
-require('sic.plugin.treesitter')
-require('sic.plugin.ts-autotag')
-require('sic.plugin.lspsaga')
-require('sic.plugin.web-devicon')
-require("sic.plugin.markdown")
-require'lspconfig'.pyright.setup{}
-require('rest-nvim').setup()
-require("sic.plugin.colorizer")
-require'lspconfig'.volar.setup{
-  filetypes = {"vue"}
-}
-vim.g.OmniSharp_server_stdio = 1
-vim.g.OmniSharp_auto_start = 1
-vim.g.OmniSharp_highlight_types = 1
-vim.g.OmniSharp_highlight_functions = 1
-vim.g.OmniSharp_popup_position = 'peek'
+vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
+vim.g.mapleader = "\\"
+require("sic.base")
+require("sic.map")
+require("sic.lazy")
